167072a47e4905c13dc6016922c224a02aab92e0
yudada31 <43599676+yudada31@users.noreply.github.com>
2018-12-18 10:39:07 +0100

    Modification on usage of sharps

---
+++ motor.cpp
@@ -0,0 +1,82 @@
+#ifdef IAM_QBOUGE
+
+#include "mbed.h"
+
+#include "common/utils.h"
+#include "config.h"
+
+#include "motor.h"
+
+
+Motor::Motor(PinName pwm_pin, PinName dir_pin, bool forward_dir) : pwm_(pwm_pin), dir_(dir_pin) {
+    forward_dir_ = forward_dir;
+    last_sPwm_ = 0;
+
+    pwm_.period(0.001 * 0.1);      // 0.0001 == 10K Hz (Gali IX : 20KHz) todo define
+
+    this->setSPwm(0);
+}
+
+void Motor::setDir(bool dir) {
+    dir_ = dir ? forward_dir_ : !forward_dir_;
+}
+
+void Motor::setUPwm(float uPwm) {
+    pwm_ = uPwm;
+}
+
+void Motor::setSPwm(float sPwm) {
+    if (ABS(sPwm) < PWM_IS_ALMOST_ZERO)
+    {
+        sPwm = 0;
+        last_sPwm_ = 0;
+    }
+    else
+    {
+        float current = last_sPwm_;
+
+        sPwm = constrain(sPwm, -1, 1);
+
+        // step the raw value
+        if (ABS(sPwm - current) > PWM_STEP)
+        {
+            if (sPwm > current)
+                sPwm = current + PWM_STEP;
+            else
+                sPwm = current - PWM_STEP;
+        }
+
+        last_sPwm_ = sPwm;
+
+        // map for applying the pwm
+        sPwm = SIGN(sPwm) * map(ABS(sPwm), 0, 1, PWM_MIN, 1);
+
+        // cap for boundary checking
+        sPwm = constrain(sPwm, -PWM_MAX, PWM_MAX);
+    }
+
+    this->setUPwm(ABS(sPwm));
+    this->setDir(sPwm >= 0);
+}
+
+bool Motor::getDir(void) {
+    return dir_ == forward_dir_;
+}
+
+float Motor::getUPwm(void) {
+    return pwm_.read();
+}
+
+float Motor::getSPwm(void) {
+    return this->getDir() ? this->getUPwm() : -this->getUPwm();
+}
+
+void Motor::updateSpeed(float mm_since_last_loop) {
+    speed_ = mm_since_last_loop / ASSERV_DELAY;
+}
+
+float Motor::getSpeed(void) {
+    return speed_;
+}
+
+#endif // #ifdef IAM_QBOUGE
---
+++ motor.h
@@ -0,0 +1,94 @@
+
+
+#include "mbed.h"
+    
+#define PWM_MIN                 0.08                       // pwm value at which the robot starts moving
+#define PWM_MAX                 0.8//1.00                        // should always be 1. Maximum PWM should be set by MC
+
+// Pwm goes from 0 to 1 over a X sec timespan.
+// This is used to increment slowly the speed, to prevent the wheels to slip
+// Note: be sure to have a steep increase (low PWM_STEP_1_AFTER_X_SEC value),
+// otherwise it might fucked up the PID settings
+#define PWM_STEP_1_AFTER_X_SEC  0.25
+// This is acceleration and should not be constant, but be incremented -> https://www.rcva.fr/10-ans-dexperience/4/
+#define PWM_STEP                (1.0/(ASSERV_FPS*PWM_STEP_1_AFTER_X_SEC))
+
+#define PWM_IS_ALMOST_ZERO      (PWM_STEP/2)                // Pwm under this value is considered to be 0
+
+/*
+    Forward and backward direction (value of the digital input `direction` of
+    the H bridge) for each motor.
+
+    Wheels must turn in opposite directions to move the robot forward or backwards.
+    This can be set via the code (here, from code to H bridge), or in the PCB
+    routing (from H bridge to plug), or in the motor wiring (plug to motor).
+*/
+#define MOTOR_DIR_LEFT_FORWARD      1
+#define MOTOR_DIR_LEFT_BACKWARD     0
+#define MOTOR_DIR_RIGHT_FORWARD     0
+#define MOTOR_DIR_RIGHT_BACKWARD    1
+
+
+class Motor{
+
+public:
+    Motor(PinName pwm_pin, PinName dir_pin, bool forward_dir);
+
+    /*
+        Set direction.
+            false (0): backward
+            true (1): forward
+    */
+    void setDir(bool dir);
+
+    /*
+        Set unsigned PWM value.
+        Value between 0 and 1.
+    */
+    void setUPwm(float uPwm);
+
+    /*
+        Set speed (direction and pwm).
+        Value between -1 and +1.
+    */
+    void setSPwm(float sPwm);
+
+    /*
+        Get direction.
+            false (0): backward
+            true (1): forward
+    */
+    bool getDir(void);
+
+    /*
+        Set unsigned PWM value.
+        Value between 0 and 1.
+    */
+    float getUPwm(void);
+
+    /*
+        Get theorical speed (direction and pwm).
+        Value between -1 and +1.
+    */
+    float getSPwm(void);
+
+    /*
+        Compute and save the actual speed of the wheel. This value is computed
+        from encoder ticks.
+    */
+    void updateSpeed(float mm_since_last_loop);
+
+    /*
+        Return the computed speed.
+        Unit: mm/sec
+    */
+    float getSpeed(void);
+
+protected:
+    PwmOut pwm_;
+    DigitalOut dir_;
+    bool forward_dir_;
+    float speed_;  // unit: mm/sec
+    float last_sPwm_;
+};
+
